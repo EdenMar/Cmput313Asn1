bc0515ac50d2e90f2de3c8e8cf9c74ee4d7d36c8
Chase McCarty <cmccarty@cs.ualberta.ca>
2014-09-17 14:59:51 -0600

    Added arg error checking

--- asn1.py
+++ asn1.py
@@ -9,17 +9,31 @@ e = None # probability that a bit is an error
 R = None # Lenght of simulation in bit time units (derived using A)
 T = 5 #
 
+
+#	Functions
+def main():
+	# Handle Arguments
+	checkArgs()
+	handleArgs()
+	printVals()
+
+# Check for bad arguments
+def checkArgs():
+	if len(sys.argv) < 7:
+		print("Not enough args")
+		exit()
+
+#handle arguments
+def handleArgs():
+	A = sys.argv[1]
+	K = sys.argv[2]
+	F = sys.argv[3]
+	e = sys.argv[4]
+	R = sys.argv[5]
+	T = sys.argv[6]
+
 # Print Values out
 def printVals():
 	print("A=", A, "K=", K, "F=", F, "e=", e, "R=", R, "T=", T)
 
-# Handle Arguments
-A = sys.argv[1]
-K = sys.argv[2]
-F = sys.argv[3]
-e = sys.argv[4]
-R = sys.argv[5]
-T = sys.argv[6]
-
-
-printVals()
+main()
