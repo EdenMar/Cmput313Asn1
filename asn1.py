9c6661b69bf7f9703d9352c639c9fb9897e70fbd
Chase McCarty <cmccarty@cs.ualberta.ca>
2014-09-24 20:14:03 -0600

    added check bits

--- asn1.py
+++ asn1.py
@@ -138,7 +138,10 @@ def isFrameGood():
 	global K
 	global F
 	blocks = K
-	blockSize = int(F/K) # 						# we need to add r here!
+	blockSize = int(F/K)
+
+	#add r to blocksize
+	blocksize = blocksize + getCheckBits(blocksize)
 	
 	#for each block
 	for i in range(blocks):
@@ -198,6 +201,30 @@ def computeThroughput():
 		return 0
 
 	return ((F * correctlyRecievedFrames) / countTimeUnits)
+
+# Returns number of HSBC check bits for supplied block size
+def getCheckBits(blockSize):
+	position = 1
+	rBits = 0
+	kBits = 0
+	K = blockSize
+
+	while kBits <= K: 	# may just need to be less than
+		if isPowOfTwo(position):
+			rBits += 1
+		else:
+			kBits += 1
+		position += 1
+	return rBits
+
+#returns true if Val is a power of 2
+def isPowOfTwo(val):
+	powers = (1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328, 281474976710656, 562949953421312, 1125899906842624)
+	if val in powers:
+		return True
+	else:
+		return False
+
 
 main()
 
